2f2e9532ac61459fdaef783b0b7957bb487c2f41
ethboi <you@example.com>
2022-11-16 21:42:13 +0100

    updates

--- src/templates/trade.ts
+++ src/templates/trade.ts
@@ -6,15 +6,13 @@ import { TradeResult } from '../types/trade'
 export function TradeTelegram(arb: Arb, tradeResult: TradeResult, strategy: Strategy, size: number, isBuy: boolean) {
   const post: string[] = []
   post.push(`${BuySellSymbol(isBuy)} ${isBuy ? 'Buy' : 'Sell'} $${strategy.market.toUpperCase()}\n`)
-  post.push(`<i>Size:</i> ${size}\n`)
-  post.push(`<i>Strike:</i> $${FN(arb.strike, 0)}\n`)
-  post.push(`<i>Exp:</i> $${FormattedDateShort(new Date(arb.expiration))}\n`)
-  post.push(`<i>Type:</i> ${arb.type}\n\n`)
-  post.push(`<i>Success?:</i> ${tradeResult.isSuccess}\n`)
-  post.push(`<i>Provider:</i> ${tradeResult.provider}\n`)
-  post.push(`<i>Price Per Option:</i> ${tradeResult.pricePerOption}\n`)
+  post.push(`Size: ${size} options\n`)
+  post.push(`$${FN(arb.strike, 0)} ${FormattedDateShort(new Date(arb.expiration))} ${arb.type}\n`)
+  post.push(`Provider: ${tradeResult.provider}\n`)
+  post.push(`Price Per Option: ${tradeResult.pricePerOption}\n\n`)
+  post.push(`Success?: ${tradeResult.isSuccess}\n`)
   if (!tradeResult.isSuccess) {
-    post.push(`<i>Fail Reason:</i> ${tradeResult.failReason}\n`)
+    post.push(`Fail Reason: ${tradeResult.failReason}\n`)
   }
   return post.join('')
 }
