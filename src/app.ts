517990e922361d58af6e28aedd617ccac49bbe6b
ethboi <you@example.com>
2022-11-17 19:14:28 +0100

    add notifier reporting

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+import { Telegraf } from 'telegraf'
 import { goBot } from './bot'
+import { PostTelegram } from './integrations/telegram'
+import { TELEGRAM_ACCESS_TOKEN } from './secrets'
 
-goBot().then(() => console.log('Arb bot is launched.'))
+async function Initialize(): Promise<void> {
+  try {
+    RegisterShutdownEvents()
+    await Notifier(false)
+    await goBot()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function Notifier(isDown = true) {
+  await PostTelegram(`Arb Bot ${isDown ? 'Down' : 'Up'}\n`, new Telegraf(TELEGRAM_ACCESS_TOKEN))
+}
+
+function RegisterShutdownEvents(): void {
+  process.on('beforeExit', async (code) => {
+    await Notifier().then(process.exit(code))
+  })
+}
+
+Initialize()
